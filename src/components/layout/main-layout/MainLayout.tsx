43c49eecd37e41c476d086b74a58701594d3cd4a
Mike <misha.o.son@gmail.com>
2025-02-14 21:48:31 +0300

    fix height

--- src/components/layout/main-layout/MainLayout.tsx
+++ src/components/layout/main-layout/MainLayout.tsx
@@ -5,7 +5,7 @@ import { ToolBar } from './tool-bar/ToolBar';
 
 export function MainLayout() {
     return (
-        <div className="flex flex-col items-center justify-between bg-[#112135] text-white min-h-screen">
+        <div className="flex flex-col items-center justify-between bg-[#112135] text-white h-[100dvh]">
             <div
                 className="w-full h-[400px] fixed top-[-30%] z-20 bg-gradient-radial pointer-events-none"
                 style={{
